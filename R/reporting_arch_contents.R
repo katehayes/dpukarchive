759095d6fa1994366646ed85ae7827aceedba828
katehayes <khayes2@sheffield.ac.uk>
2024-12-11 13:41:28 +0000

    adding a title to the file status plot

--- R/reporting_arch_contents.R
+++ R/reporting_arch_contents.R
@@ -16,6 +16,39 @@ get_force_list <- function() {
 # https://stackoverflow.com/questions/41582136/r-what-do-you-call-the-and-operators-and-how-do-they-differ
 
 
+#' Converts police force code to full name
+#'
+#' @param pf_code A string containing a police force code name
+#'
+#' @return A named character string containing the corresponding police force name
+#'
+#' @example
+#' get_pf_name(metropolitan)
+get_pf_name <- function(pf_code) {
+
+  pf_name <- get_force_list() %>%
+    filter(id == pf_code) %>%
+    select(name) %>%
+    as.character()
+
+  return(pf_name)
+
+}
+
+
+get_ds_name <- function(ds_code) {
+
+  ds_name <- case_when(ds_code == "stop-and-search" ~ "Stop & search",
+                       ds_code == "outcomes" ~ "Outcomes",
+                       ds_code == "street" ~ "Street")
+
+  return(ds_name)
+
+}
+
+
+# get_pf_name(pf_code = "metropolitan")
+
 #' Plots file presence/absence in folders
 #' @param police_force A string.
 #' @param data_series A string.
@@ -27,12 +60,16 @@ get_force_list <- function() {
 #' fs_graph(police_force = "wiltshire", data_series = "stop-and-search", month_min = "2019-01", month_max = "2024-06")
 fs_graph <- function(police_force = ".", data_series = ".", month_min = ".", month_max = ".") {
 
+  # pf_name <- get_pf_name(police_force)
+  # ds_name <- get_ds_name(data_series)
+
+
   text_colour <- "white"
 
   # folders along x axis files along y axis.
 
   report <- should_contain() %>%
-    arch_filter(police_force, data_series, month_min, month_max, option = "should_contain") %>%
+    arch_filter(police_force, data_series, month_min, month_max, option = should_contain) %>%
     select(file_name, folder, contains) %>%
     mutate(contains = if_else(contains == T, "Present", "Absent")) %>%
     # complete(file_name, folder, fill = list(contains = "outside range")) %>%
@@ -50,11 +87,13 @@ fs_graph <- function(police_force = ".", data_series = ".", month_min = ".", mon
                               expand = c(0,0)) +
     ggplot2::theme(strip.background = ggplot2::element_rect(fill='transparent'),
                    strip.text = ggplot2::element_text(colour = text_colour),
+                   plot.title = ggplot2::element_text(colour = text_colour),
                    plot.background = ggplot2::element_rect(fill='transparent'),
                    panel.background = ggplot2::element_rect(fill='transparent'),
                    legend.background =  ggplot2::element_rect(fill='transparent'),
                    axis.ticks = ggplot2::element_line(colour = text_colour),
-                   panel.grid.major = ggplot2::element_line(colour="darkgray", size = 0.1),
+                   # panel.grid.major = ggplot2::element_line(colour="darkgray", size = 0.1),
+                   panel.grid.major = ggplot2::element_blank(),
                    axis.text.y = ggplot2::element_text(size = 7, colour = text_colour),
                    axis.text.x = ggplot2::element_text(size = 7, colour = text_colour, angle=90),
                    axis.title.x = ggplot2::element_text(size = 11, colour = text_colour),
@@ -63,11 +102,21 @@ fs_graph <- function(police_force = ".", data_series = ".", month_min = ".", mon
                    legend.key.size = ggplot2::unit(0.5, "lines"),
                    legend.text = ggplot2::element_text(size = 11,
                                                        colour = text_colour),
-                   legend.position = c(0.1, 0.1))
-
-  # +ggplot2::labs(title = paste())
+                   legend.position = c(0.1, 0.1)) +
+    ggplot2::labs(title = paste("Police force:", get_pf_name(police_force),
+                                "\nData series:", get_ds_name(data_series),
+                                "\nData presence/absence for the period",
+                                month_min, "to", month_max, sep = " "))
 
 
 }
 
+# check <- fs_graph(police_force = "wiltshire", data_series = "stop-and-search", month_min = "2019-01", month_max = "2024-06")
+#
+# check
 
+
+# check <- paste("Police force:", get_pf_name("wiltshire"),
+#                "\nData series:", get_ds_name("outcomes"),
+#                "\nData presence/absence for the period",
+#                "2019-01", "to", "2024-01", sep = " ")
